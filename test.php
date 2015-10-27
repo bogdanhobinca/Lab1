6ca5dae4b04e44f827b8d1a561dee7f07db10038
bogdanstefan <b.s.hobinca@rgu.ac.uk>
2015-10-27 01:57:44 +0000

    Added new php file

--- test.php
+++ test.php
@@ -20,6 +20,7 @@
         $myage=111;
         echo "My name is ".$myname." and I am ".$myage;
     ?>
+    <br />
     <?php
         $name="Edgar";
         if ($name=="Simon"){
@@ -27,6 +28,24 @@
         }else{
             print "Who are you?";
         }
+    ?>
+    <br />
+    <?php
+        $numberOfHobbits=2;
+        switch ($numberOfHobbits){
+            case 1: echo "1 sad hobbit"; break;
+            case 2: echo "2 happy hobbits"; break;
+            case 3: echo "3 hobbits are a crowd"; break;
+            default: echo "All the hobbits have gone home";
+        }
+    ?>
+    <br />
+    <?php
+    $myArray=array("do", "re", "mi");
+    echo $myArray[0];
+    $myArray[1]="la";
+    echo $myArray[1];
+    unset($array[2]);
     ?>
 </p>
 </body>
