aaa00fc5636b5575e9cd88f9bc178dbb74f5088e
bogdanstefan <b.s.hobinca@rgu.ac.uk>
2015-10-27 23:46:43 +0000

    Added new php file

--- whileloop.php
+++ whileloop.php
@@ -3,7 +3,31 @@
 <head></head>
 <body>
 <?php
-    echo rand(0,2);
+    $specs=7;
+    $mugs=7;
+    $rolls=7;
+    $count=1;
+    while(($specs>0 || $mugs>0 || $rolls>0) && $count<30){
+        $rez="On the ".$count;
+        if ($count==1 || $count==21){
+            $rez=$rez."st";
+        }else if ($count==2){
+            $rez=$rez."nd";
+        }else if ($count==3){
+            $rez=$rez."rd";
+        }else{
+            $rez=$rez."th";
+        }
+
+        $todaysGood=rand(0,2);
+        switch($todaysGood){
+            case 0: $rez=$rez." of the month specs are available"; $specs--; break;
+            case 1: $rez=$rez." of the month mugs are available"; $mugs--; break;
+            case 2: $rez=$rez." of the month rolls are available"; $rolls--; break;
+        }
+        echo "<p>$rez</p>";
+        $count++;
+    }
 ?>
 </body>
 </html>
